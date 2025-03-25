6c466d046349c504bcc12a5c7080c0535fab952e
Gabi Moldovan <68482473+GabiMoldovan@users.noreply.github.com>
2025-03-25 20:30:02 +0200

    adaugare structura ecrane frontend

---
+++ frontend/screens/generateReports/GenerateReports.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function GenerateReports() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>&gt;:D</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'black',
+  },
+});
